dc9fb4e62c751069fa6c68df9d7cb2388c074cec
Piotr Michalski <mrpiotrmichalski@gmail.com>
2022-05-17 19:57:36 +0200

    Board and Cell implemented

---
+++ Chess/Models/Board.cs
@@ -0,0 +1,56 @@
+﻿namespace Chess.Models
+{
+    public class Board
+    {
+        public int Size { get; set; }
+        public Cell[,] Grid { get; set; }
+        public Board(int s)
+        {
+            Size = s;
+            Grid = new Cell[Size, Size];
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    Grid[i, j] = new Cell(i, j);
+                }
+            }
+        }
+
+        public void MarkNextLegalMoves( Cell currentCell, string chessPiece)
+        {
+            for(int i = 0; i < Size; ++i)
+            {
+                for( int j = 0; j < Size; ++j)
+                {
+                    Grid[i, j].LegalNextMove = false;
+                    Grid[i,j].CurrentlyOccupied = false;
+                }
+            }
+
+            switch (chessPiece)
+            {
+                case "King":
+                    break;
+                case "Queen":
+                    break;
+                case "Knight":
+                    Grid[currentCell.RowNumber + 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    Grid[currentCell.RowNumber + 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    Grid[currentCell.RowNumber - 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    Grid[currentCell.RowNumber - 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    Grid[currentCell.RowNumber + 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
+                    Grid[currentCell.RowNumber + 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+                    Grid[currentCell.RowNumber - 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
+                    Grid[currentCell.RowNumber - 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+                    break;
+                case "Rook":
+                    break;
+                case "Bishop":
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Chess/Models/Cell.cs
@@ -0,0 +1,16 @@
+﻿namespace Chess.Models
+{
+    public class Cell
+    {
+        public int RowNumber { get; set; }
+        public int ColumnNumber { get; set; }
+        public bool CurrentlyOccupied { get; set; }
+        public bool LegalNextMove { get; set; }
+
+        public Cell(int x, int y)
+        {
+            RowNumber = x;
+            ColumnNumber = y;
+        }
+    }
+}
