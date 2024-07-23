c1884537fe23eb8494add050db7b0ab30947f465
Piotr Michalski <mrpiotrmichalski@gmail.com>
2024-07-23 17:48:47 +0200

    CellTest

--- Chess.UnitTests/CellTests.cs
+++ Chess.UnitTests/CellTests.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
+using Chess.Models;
+using NUnit.Framework.Legacy;
 
 namespace Chess.UnitTests
 {
-    internal class CellTests
+    public class CellTests
     {
+        [Test]
+        public void Test_CellColorChange()
+        {
+            // Arrange
+            var cell = new Cell(0, 0, Color.White);
+
+            // Act
+            cell.FieldColor = "#FFC300";
+
+            // Assert
+            ClassicAssert.AreEqual("#FFC300", cell.FieldColor);
+        }
     }
 }
